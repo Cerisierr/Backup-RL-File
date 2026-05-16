86c1b64ef466e74f7335c48482c7e90e461b0724
Cerisierr_ <135485169+Cerisierr@users.noreply.github.com>
2026-05-16 23:16:48 +0200

    Error Update

--- stats/script.js
+++ stats/script.js
@@ -3,33 +3,34 @@ async function loadStats() {
     try {
 
         const response = await fetch(
-            "https://cerisier.goatcounter.com/api/v0/stats/total",
+            "https://cerisier.goatcounter.com/api/v0/stats/hits",
             {
                 headers: {
-                    Authorization: "Bearer 120a6j6i1q2eo10d1k1j06h768wru2u4ifaxnv179uhsj5fvww3"
+                    Authorization:
+                        "Bearer 120a6j6i1q2eo10d1k1j06h768wru2u4ifaxnv179uhsj5fvww3"
                 }
             }
         );
 
         const data = await response.json();
 
-        // Visiteurs uniques
-        document.getElementById("visitors").innerText =
-            data.visitors || 0;
+        console.log(data);
 
-        // Pages vues
         document.getElementById("downloads").innerText =
-            data.hits || 0;
+            data.total ?? 0;
+
+        document.getElementById("visitors").innerText =
+            data.total ?? 0;
 
     } catch (error) {
 
         console.error(error);
 
-        document.getElementById("visitors").innerText =
-            "Error";
-
         document.getElementById("downloads").innerText =
-            "Error";
+            "0";
+
+        document.getElementById("visitors").innerText =
+            "0";
     }
 }
 
